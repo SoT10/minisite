9d0c2105945c72f3c89795303f89972bdb08aa8d
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-11 20:40:32 +0300

    Add files via upload

--- minisforum/minisforum/settings.py
+++ minisforum/minisforum/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jak%hz02pk@x$m5i_kn*2a9$p@v8ck&c8a^aiean4bawh6s^sb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
