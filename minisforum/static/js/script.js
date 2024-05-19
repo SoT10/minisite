20f1f02c303f6e46a7ddfd9648f0f592922d4796
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-19 17:48:28 +0300

    1

---
+++ minisforum/static/js/script.js
@@ -0,0 +1,64 @@
+const home_next = document.getElementById('arrow_right');
+const home_return = document.getElementById('arrow_left');
+
+function home_next_slide(number){
+	var slide1=document.getElementById("home_slider1")
+	var slide2=document.getElementById("home_slider2")
+	var slide3=document.getElementById("home_slider3")
+	var slide4=document.getElementById("home_slider4")
+	if (number==1) {
+		slide1.style.display="block"
+		slide2.style.display="none"
+		slide3.style.display="none"
+		slide4.style.display="none"
+	}
+	if (number==2) {
+		slide1.style.display="none"
+		slide2.style.display="block"
+		slide3.style.display="none"
+		slide4.style.display="none"
+	}
+	if (number==3) {
+		slide1.style.display="none"
+		slide2.style.display="none"
+		slide3.style.display="block"
+		slide4.style.display="none"
+	}
+	if (number==4) {
+		slide1.style.display="none"
+		slide2.style.display="none"
+		slide3.style.display="none"
+		slide4.style.display="block"
+	}
+};
+
+function home_return_slide(number){
+	var slide1=document.getElementById("home_slider1")
+	var slide2=document.getElementById("home_slider2")
+	var slide3=document.getElementById("home_slider3")
+	var slide4=document.getElementById("home_slider4")
+	if (number==1) {
+		slide1.style.display="block"
+		slide2.style.display="none"
+		slide3.style.display="none"
+		slide4.style.display="none"
+	}
+	if (number==2) {
+		slide1.style.display="none"
+		slide2.style.display="block"
+		slide3.style.display="none"
+		slide4.style.display="none"
+	}
+	if (number==3) {
+		slide1.style.display="none"
+		slide2.style.display="none"
+		slide3.style.display="block"
+		slide4.style.display="none"
+	}
+	if (number==4) {
+		slide1.style.display="none"
+		slide2.style.display="none"
+		slide3.style.display="none"
+		slide4.style.display="block"
+	}
+};
