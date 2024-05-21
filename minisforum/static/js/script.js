cee3ef45773a4ee530c25e7c0bf891991cc15b68
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-21 22:12:04 +0300

    home

--- minisforum/static/js/script.js
+++ minisforum/static/js/script.js
@@ -1,3 +1,5 @@
+/*Начало блока home*/
+
 const home_next = document.getElementById('arrow_right');
 const home_return = document.getElementById('arrow_left');
 
@@ -63,44 +65,5 @@ function home_return_slide(number){
 	}
 };
 
-document.querySelectorAll('.home_lupa1').forEach(function(img) {
-    img.addEventListener('mouseenter', function() {
-        this.style.display = 'none';
-        this.nextElementSibling.style.display = 'block';
-    });
-});
 
-document.querySelectorAll('.home_lupa2').forEach(function(img) {
-    img.addEventListener('mouseleave', function() {
-        this.style.display = 'none';
-        this.previousElementSibling.style.display = 'block';
-    });
-});
-
-document.querySelectorAll('.home_heart1').forEach(function(img) {
-    img.addEventListener('mouseenter', function() {
-        this.style.display = 'none';
-        this.nextElementSibling.style.display = 'block';
-    });
-});
-
-document.querySelectorAll('.home_heart2').forEach(function(img) {
-    img.addEventListener('mouseleave', function() {
-        this.style.display = 'none';
-        this.previousElementSibling.style.display = 'block';
-    });
-});
-
-document.querySelectorAll('.home_korzina1').forEach(function(img) {
-    img.addEventListener('mouseenter', function() {
-        this.style.display = 'none';
-        this.nextElementSibling.style.display = 'block';
-    });
-});
-
-document.querySelectorAll('.home_korzina2').forEach(function(img) {
-    img.addEventListener('mouseleave', function() {
-        this.style.display = 'none';
-        this.previousElementSibling.style.display = 'block';
-    });
-});
+/*Конец блока home*/
