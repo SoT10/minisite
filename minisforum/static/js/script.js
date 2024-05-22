1fd5c59867fbccc83c0c5a372a7429b1802a04a4
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-22 19:09:15 +0300

    1

--- minisforum/static/js/script.js
+++ minisforum/static/js/script.js
@@ -64,6 +64,42 @@ function home_return_slide(number){
 		slide4.style.display="block"
 	}
 };
-
-
 /*Конец блока home*/
+
+/*начало блока shop*/
+function show_close_shop() {
+	var shop_cont=document.getElementById('shop_cont');
+	var full_shop=document.getElementById('full_shop');
+	var shop=document.getElementById('shop');
+	var dark_back=document.getElementById('dark_back');
+	if (dark_back.style.opacity=="1") {
+		dark_back.style.opacity="0"
+		shop_cont.classList.remove('shop_slide_in')
+		shop_cont.classList.add("shop_slide_out")
+		full_shop.classList.remove('shop_slide_in')
+		full_shop.classList.add("shop_slide_out")
+		shop.style.right="12px"
+	} else {
+		dark_back.style.opacity="1"
+		shop_cont.classList.remove('shop_slide_out')
+		shop_cont.classList.add("shop_slide_in")
+		full_shop.classList.remove('shop_slide_out')
+		full_shop.classList.add("shop_slide_in")
+		shop.style.right="344px"
+	}
+}
+
+function close_shop() {
+	var shop_cont=document.getElementById('shop_cont');
+	var full_shop=document.getElementById('full_shop');
+	var shop=document.getElementById('shop');
+	var dark_back=document.getElementById('dark_back');
+	dark_back.style.opacity="0"
+	shop_cont.classList.remove('shop_slide_in')
+	shop_cont.classList.add("shop_slide_out")
+	full_shop.classList.remove('shop_slide_in')
+	full_shop.classList.add("shop_slide_out")
+	shop.style.right="12px"
+}
+/*конец блока shop*/
+
