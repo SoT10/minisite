2ff2d5fb2e03d73200b76cc0c4384fc66a72117b
SecureSo10 <40361673+SoT10@users.noreply.github.com>
2024-05-22 19:09:34 +0300

    12

--- minisforum/howto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from howto import views
-
-app_name = 'howto'
-
-urlpatterns = [
-    path('', views.howto, name='howto'),
-]
--- minisforum/howto/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def howto(request):
-    context = {
-        'title': 'Как сделать',
-    }
-    template_name = 'howto/howto.html'
-    return render(request, template_name, context)
