7018e0d11a41222b084a7c513300924708bb88a4
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-05-23 19:18:34 +0300

    1

--- minisforum/minisforum/settings.py
+++ minisforum/minisforum/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'minisforum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'minisite',
+        'USER': 'admin2024',
+        'PASSWORD': 'R00x8dDRx84b',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
