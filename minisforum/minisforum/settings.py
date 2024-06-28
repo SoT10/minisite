de3540f650dc79f08fafd75bb507e993404c2b93
Comrade <59848910+artemka201601@users.noreply.github.com>
2024-06-28 18:33:31 +0300

    123

--- minisforum/minisforum/settings.py
+++ minisforum/minisforum/settings.py
@@ -145,4 +145,14 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 LOGIN_URL = '/my_accaunt/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'minisforum-russia@yandex.ru'
+EMAIL_HOST_PASSWORD = 'glbkawaxfllciqfm'
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
